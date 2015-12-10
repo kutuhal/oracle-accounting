3ea01394ddd3a95c015bab6fdd0b483fb3d01844
kutuhal <sdgogikar@gmail.com>
2015-12-10 23:49:24 +0530

    Accounting model created and admin interface changed

--- p2p/admin.py
+++ p2p/admin.py
@@ -1,3 +1,10 @@
 from django.contrib import admin
+from p2p.models import  P2P_accounting
 
 # Register your models here.
+
+class AccountingAdmin(admin.ModelAdmin):
+    fields = ['dr_cr', 'account_description', 'accounting_entry','item_type']
+    list_display = ('dr_cr', 'account_description', 'accounting_entry','item_type')
+
+admin.site.register(P2P_accounting, AccountingAdmin)
