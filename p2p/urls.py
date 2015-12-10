4dfd9190c1b3ea5f103e9fd0b5c6f383627a111b
kutuhal <sdgogikar@gmail.com>
2015-12-11 00:15:46 +0530

    template created and showing accounting entries list

--- p2p/urls.py
+++ p2p/urls.py
@@ -1,11 +1,10 @@
 from django.conf.urls import include, url
-from django.contrib import admin
+from . import views
 
 urlpatterns = [
     # Examples:
     # url(r'^$', 'accounting.views.home', name='home'),
     # url(r'^blog/', include('blog.urls')),
 
-    url(r'^admin/', include(admin.site.urls)),
-    url(r'^p2p_accounting/', include('p2p.urls')),
+    url(r'^$', views.p2p_accounting, name='p2p_accounting'),
 ]
--- p2p/views.py
+++ p2p/views.py
@@ -1,3 +1,7 @@
 from django.shortcuts import render
+from .models import P2P_accounting
 
 # Create your views here.
+def p2p_accounting(request):
+    accounting = P2P_accounting.objects.all()
+    return render(request, 'p2p/p2p_accounting.html',{'p2p_accounting': accounting })
