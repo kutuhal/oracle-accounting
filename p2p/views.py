2e4c66c5eb5b7ef0d249c3374034f8321f53e2a4
kutuhal <sdgogikar@gmail.com>
2015-12-11 15:08:15 +0530

    New template for A/cing of Inventory 'Item Type'

--- p2p/views.py
+++ p2p/views.py
@@ -11,6 +11,5 @@ def p2p_accounting(request):
 def inv_accounting(request):
     receipt_accting = P2P_accounting.objects.filter( accounting_entry='PO Receipt').filter(item_type='Inventory')
     deliver_accting = P2P_accounting.objects.filter( accounting_entry='PO Deliver').filter(item_type='Inventory')
-    return render(request, 'p2p/p2p_accounting.html',
+    return render(request, 'p2p/inv_accounting.html',
         {'po_receipt_accting': receipt_accting, 'po_deliver_accting' : deliver_accting})
-    return render(request,'p2p/p2p_accounting.html',{})
