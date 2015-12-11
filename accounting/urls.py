72183f6d67c075556bb6f2390f37292ae1efb7de
kutuhal <sdgogikar@gmail.com>
2015-12-11 11:38:29 +0530

    created home.html and used bootstrap accordian

--- accounting/urls.py
+++ accounting/urls.py
@@ -6,7 +6,7 @@ urlpatterns = [
     # Examples:
     # url(r'^$', 'accounting.views.home', name='home'),
     # url(r'^blog/', include('blog.urls')),
-    url(r'^$', TemplateView.as_view(template_name="base.html"), name='index'),
+    url(r'^$', TemplateView.as_view(template_name="home.html"), name='home'),
     url(r'^admin/', include(admin.site.urls)),
     url(r'^p2p_accounting/', include('p2p.urls')),
 ]
