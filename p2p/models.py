9e2f209047b816ae79478bacf3c19052edddcf99
kutuhal <sdgogikar@gmail.com>
2015-12-11 15:33:08 +0530

    reformatted A/cing table column width; added AP Invoice & Payment A/cing

--- p2p/models.py
+++ p2p/models.py
@@ -6,5 +6,5 @@ class P2P_accounting(models.Model):
     dr_cr = models.CharField (max_length = 6)
     account_description = models.CharField (max_length=25)
     accounting_entry = models.CharField(max_length=15)
-    item_type = models.CharField(max_length=10)
+    item_type = models.CharField(max_length=20)
     stream = models.CharField(max_length=5)
--- p2p/views.py
+++ p2p/views.py
@@ -5,11 +5,17 @@ from .models import P2P_accounting
 def p2p_accounting(request):
     receipt_accting = P2P_accounting.objects.filter( accounting_entry='PO Receipt').filter(item_type='Expense')
     deliver_accting = P2P_accounting.objects.filter( accounting_entry='PO Deliver').filter(item_type='Expense')
+    invoice_accting = P2P_accounting.objects.filter( accounting_entry='AP Invoice')
+    payment_accting = P2P_accounting.objects.filter( accounting_entry='AP Payment')
     return render(request, 'p2p/p2p_accounting.html',
-        {'po_receipt_accting': receipt_accting, 'po_deliver_accting' : deliver_accting})
+        {'po_receipt_accting': receipt_accting, 'po_deliver_accting' : deliver_accting,
+        'ap_invoice_accting':invoice_accting, 'ap_payment_accting':payment_accting})
 
 def inv_accounting(request):
     receipt_accting = P2P_accounting.objects.filter( accounting_entry='PO Receipt').filter(item_type='Inventory')
     deliver_accting = P2P_accounting.objects.filter( accounting_entry='PO Deliver').filter(item_type='Inventory')
+    invoice_accting = P2P_accounting.objects.filter( accounting_entry='AP Invoice')
+    payment_accting = P2P_accounting.objects.filter( accounting_entry='AP Payment')
     return render(request, 'p2p/inv_accounting.html',
-        {'po_receipt_accting': receipt_accting, 'po_deliver_accting' : deliver_accting})
+        {'po_receipt_accting': receipt_accting, 'po_deliver_accting' : deliver_accting,
+        'ap_invoice_accting':invoice_accting, 'ap_payment_accting':payment_accting})
