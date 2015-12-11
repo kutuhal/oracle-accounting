f5159ddb58b952b7bc967c867c760e462bb50c29
kutuhal <sdgogikar@gmail.com>
2015-12-11 12:48:21 +0530

    added bootstrap panels;used filter to separate a/cing entries

--- p2p/views.py
+++ p2p/views.py
@@ -3,5 +3,7 @@ from .models import P2P_accounting
 
 # Create your views here.
 def p2p_accounting(request):
-    accounting = P2P_accounting.objects.all()
-    return render(request, 'p2p/p2p_accounting.html',{'p2p_accounting': accounting })
+    receipt_accting = P2P_accounting.objects.filter( accounting_entry='PO Receipt')
+    deliver_accting = P2P_accounting.objects.filter( accounting_entry='PO Deliver')
+    return render(request, 'p2p/p2p_accounting.html',
+        {'po_receipt_accting': receipt_accting, 'po_deliver_accting' : deliver_accting})
