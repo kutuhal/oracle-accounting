0c97854985642b5a1695b0c875c1294ef6e3570a
kutuhal <sdgogikar@gmail.com>
2015-12-11 09:25:17 +0530

    included bootstrap and added base template

--- accounting/urls.py
+++ accounting/urls.py
@@ -1,11 +1,12 @@
 from django.conf.urls import include, url
 from django.contrib import admin
+from django.views.generic import TemplateView
 
 urlpatterns = [
     # Examples:
     # url(r'^$', 'accounting.views.home', name='home'),
     # url(r'^blog/', include('blog.urls')),
-
+    url(r'^$', TemplateView.as_view(template_name="base.html"), name='index'),
     url(r'^admin/', include(admin.site.urls)),
     url(r'^p2p_accounting/', include('p2p.urls')),
 ]
