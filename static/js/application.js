5be22e3cf6ac1bd7519d7366a6ad445ee04e2216
kutuhal <sdgogikar@gmail.com>
2015-12-13 00:35:47 +0530

    Added bootstrap modal in template & used JQuery to show - if inventory item + period end accrual

--- static/js/application.js
+++ static/js/application.js
@@ -1,7 +1,8 @@
 
 $("#form_filter").submit(function() {
       if ($('#id_item_type').val() == 'Inventory'  & $('#id_period_end_accrual').val()=='True') {
-        $('#error_message').text ('For an Inventory Item, we cannot enable "Period-end Accrual"');
+        //$('#error_message').text ('For an Inventory Item, we cannot enable "Period-end Accrual"');
+        $('#myModal').modal('show');
         return false;
 
       }
