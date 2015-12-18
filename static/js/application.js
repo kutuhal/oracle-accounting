d31ef446f82e8bea4dab63586fb64e2e23c9b180
kutuhal <sdgogikar@gmail.com>
2015-12-18 07:42:03 +0530

    Adding Button to toggle display of Sidebar

--- static/js/application.js
+++ static/js/application.js
@@ -15,3 +15,17 @@ $("#form_filter").submit(function() {
 $("#add-field").click(function() {
         $('#add-field-form').toggle(100)
     });
+
+// Toggle Sidebar and increase width of main accordingly
+$('.sidebar-toggle').click(function(event) {
+	/* Act on the event */
+	console.log ($(".sidebar").attr('display'));
+	$ ('.sidebar').toggle();
+	if($('.sidebar').css('display') == 'none') {
+		
+		$(".main").attr("class", "col-sm-9 col-sm-offset-0 col-md-12 col-md-offset-0 main");
+	} else {
+		$(".main").attr("class", "col-sm-8 col-sm-offset-3 col-md-9 col-md-offset-3 main");
+	}
+	
+});
