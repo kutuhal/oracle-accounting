48a295a2883b4d1e2a8d902a9f76bbea015bb112
kutuhal <sdgogikar@gmail.com>
2015-12-18 09:11:57 +0530

    Moved Item type choices from Form to Model

--- p2p/models.py
+++ p2p/models.py
@@ -6,12 +6,16 @@ class P2P_accounting(models.Model):
     DR_OR_CR = ( ('DEBIT','DEBIT'), ('CREDIT','CREDIT'))    
     YES_OR_NO = ((True, 'Yes'), (False, 'No'))
     OE_LINE_FLOW = (('Bill Only', 'Bill Only'),('Bill Only with Inv Interface', 'Bill Only with Inv Interface') )
+    ITEM_TYPES = ( ('Expense','Expense'), ('Inventory','Inventory'))    
     dr_cr = models.CharField (max_length = 6, 
                                                     choices = DR_OR_CR,
                                                     blank=False)
     account_description = models.CharField (max_length=30)
     accounting_entry = models.CharField(max_length=25)
-    item_type = models.CharField(max_length=20)
+    item_type = models.CharField(max_length=20,
+                                                            choices= ITEM_TYPES,
+                                                            default= 'Expense',
+                                                            blank= False)
     stream = models.CharField(max_length=5)
     period_end_accrual = models.BooleanField(
                                                             default = False,
